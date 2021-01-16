a5329ae8702eea12d2154128aef3dc509a8eaa27
ajs2104@gmail.com <ajs2104@gmail.com>
2021-01-16 15:34:46 -0300

    Trabalhando com Specs

---
+++ MyStore.Domain/Account/Specs/UserSpecs.cs
@@ -0,0 +1,23 @@
+﻿using MyStore.Domain.Account.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStore.Domain.Account.Specs
+{
+    public static class UserSpecs
+    {
+        public static Expression<Func<User, bool>> GetUserByName(string username)
+        {
+            return x => x.Username == username;
+        }
+
+        public static Expression<Func<User, bool>> GetByAutorizationCode(string authorizationCode)
+        {
+            return x => x.AuthorizationCode == authorizationCode;
+        }
+    }
+}
