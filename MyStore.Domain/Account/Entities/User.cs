a8976e33240e4c4d6f68fd834e5c60523c454b0c
ajs2104@gmail.com <ajs2104@gmail.com>
2021-01-16 15:23:03 -0300

    Isolando as Validacoes em Escopos

--- MyStore.Domain/Account/Entities/User.cs
+++ MyStore.Domain/Account/Entities/User.cs
@@ -1,4 +1,5 @@
 ﻿using MyStore.Domain.Account.Enums;
+using MyStore.Domain.Account.Scopes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -39,40 +40,25 @@ namespace MyStore.Domain.Account.Entities
 
         public void Register()
         {
-            Password = EncryptPassword();
+            this.RegisterScopeIsValid();
+            Password = EncryptPassword(Password);
         }
-
-        private string EncryptPassword()
-        {
-            throw new NotImplementedException();
-        }
-
+       
         public void Verify(string verificationCode)
         {
-            if (verificationCode == VerificationCode)
-                Verified = true;
+            this.VerificationScopeIsValid(verificationCode);
+            Verified=true;
         }   
 
         public void Activate(string activationCode)
         {
-            if (!Verified)
-                return;
-
-            if (activationCode == ActivationCode)
-                Active = true;
+            this.ActivationScopeIsValid(activationCode);
+            Active = (activationCode == ActivationCode);
         }
 
         public void RequestLogin(string username)
         {
-            if (!Active)
-                return;
-
-            if (!Verified)
-                return;
-
-            if (username.ToLower() != Username.ToLower())
-                return;
-
+            this.RequestLoginScopeIsValid(username);
             AuthorizationCode = GenerateAutorizationCode();
             LastAuthorizationCodeRequest = DateTime.Now;
         }
@@ -82,18 +68,28 @@ namespace MyStore.Domain.Account.Entities
             return Guid.NewGuid().ToString().Substring(0, 4).ToUpper();
         }
 
-        public bool Authenticate(string authorizationCode, string password)
+        public void Authenticate(string authorizationCode, string password)
         {
-            if (!Active)
-                return false;
+            this.LoginScopeIsValid(authorizationCode, password);
+            LastLoginDate = DateTime.Now;
+        }
 
-            if (!Verified)
-                return false;
+        public string EncryptPassword(string pass)
+        {
+            if (!String.IsNullOrEmpty(Password))
+            {
+                var password = String.Empty;
+                password = (pass += "|2d331cca-f6c0-40c0-bb43-6e32989c2881");
+                System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create();
+                byte[] data = md5.ComputeHash(System.Text.Encoding.Default.GetBytes(password));
+                System.Text.StringBuilder sbString = new System.Text.StringBuilder();
+                for (int i = 0; i < data.Length; i++)
+                    sbString.Append(data[i].ToString("x2"));
 
-            if (authorizationCode != AuthorizationCode || password != Password)
-                return false;
+                return sbString.ToString();
+            }
 
-            return true;
+            return "";
         }
     }
 }
---
+++ MyStore.Domain/Account/Scopes/UserScopes.cs
@@ -0,0 +1,69 @@
+﻿using DomainNotificationHelper.Validation;
+using MyStore.Domain.Account.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStore.Domain.Account.Scopes
+{
+    public static class UserScopes
+    {
+        public static bool RegisterScopeIsValid(this User user)
+        {
+            return AssertionConcern.IsSatisfiedBy
+                (
+                    AssertionConcern.AssertLength(user.Username, 6, 20, "O usuario deve conter entre 6 e 20 caracteres"),
+                    AssertionConcern.AssertLength(user.Password, 6, 20, "A senha deve conter entre 6 e 20 caracteres")    
+                );
+        }   
+
+        public static bool VerificationScopeIsValid(this User user, string verificationCode)
+        {
+            return AssertionConcern.IsSatisfiedBy
+                (
+                    AssertionConcern.AssertNotNull(user,"Nenhum usuario encontrado"),
+                    AssertionConcern.AssertTrue(user.Verified == false,"Usuario ja verificado"),
+                    AssertionConcern.AssertAreEquals(user.VerificationCode, verificationCode,"Codigos de verificaçaõ diferentes")
+                );
+        }
+
+        public static bool ActivationScopeIsValid(this User user, string activationCode)
+        {
+            return AssertionConcern.IsSatisfiedBy
+                (
+                    AssertionConcern.AssertNotNull(user, "Nenhum usuario encontrado"),
+                    AssertionConcern.AssertTrue(user.Verified == true, "E-mail não verificado"),
+                    AssertionConcern.AssertTrue(user.Active == false, "Cadastro já verificado"),
+                    AssertionConcern.AssertAreEquals(user.ActivationCode, activationCode, "Codigos de ativação diferente")
+                );
+        }
+
+        public static bool RequestLoginScopeIsValid(this User user, string userName)
+        {
+            return AssertionConcern.IsSatisfiedBy
+                (
+                    AssertionConcern.AssertNotNull(user, "Nenhum usuario encontrado"),
+                    AssertionConcern.AssertTrue(user.Verified == true, "E-mail não verificado"),
+                    AssertionConcern.AssertTrue(user.Active == true, "Cadastro não verificado"),
+                    AssertionConcern.AssertAreEquals(user.Username.ToLower(), userName.ToLower(), "Nome de usuario diferente"),
+                    AssertionConcern.AssertAreEquals(DateTime.Compare(user.LastAuthorizationCodeRequest.AddMinutes(-5),DateTime.Now).ToString(), (-1).ToString(), "Um SMS já foi enviado, aguarde 5 minutos")
+                );
+        }
+
+        public static bool LoginScopeIsValid(this User user, string authorizationCode, string password)
+        {
+            return AssertionConcern.IsSatisfiedBy
+                (
+                    AssertionConcern.AssertNotNull(user, "Nenhum usuario encontrado"),
+                    AssertionConcern.AssertTrue(user.Verified == true, "E-mail não verificado"),
+                    AssertionConcern.AssertTrue(user.Active == true, "Cadastro não ativado"),
+                    AssertionConcern.AssertAreEquals(user.AuthorizationCode.ToLower(), authorizationCode.ToLower(), "Codigo de Autorização diferente"),
+                    AssertionConcern.AssertAreEquals(user.Password.ToLower(), password.ToLower(), "Codigo de Autorização diferente"),
+                    AssertionConcern.AssertAreEquals(DateTime.Compare(user.LastAuthorizationCodeRequest.AddMinutes(5), DateTime.Now).ToString(), (1).ToString(), "Codigo de autenticação expirado")
+                );
+        }
+
+    }
+}
