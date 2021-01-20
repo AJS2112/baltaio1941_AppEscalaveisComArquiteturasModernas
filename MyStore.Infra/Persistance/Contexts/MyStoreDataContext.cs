486d356ea7d3cd49d478569092ba78fc5c0fec8f
ajs2104@gmail.com <ajs2104@gmail.com>
2021-01-20 20:26:58 -0300

    Trabalhando com ORMs

---
+++ MyStore.Infra/Persistance/Contexts/MyStoreDataContext.cs
@@ -0,0 +1,27 @@
+﻿using MyStore.Domain.Account.Entities;
+using MyStore.Infra.Persistance.Mappings;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStore.Infra.Persistance.Contexts
+{
+    public class MyStoreDataContext : DbContext
+    {
+        public MyStoreDataContext() : base("MyStoreConnectionString")
+        {
+            Configuration.LazyLoadingEnabled = false;
+            Configuration.ProxyCreationEnabled = false;
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new UserMappings());
+        }
+    }
+}
