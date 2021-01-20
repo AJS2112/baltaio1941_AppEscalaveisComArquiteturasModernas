200788fe17c37a4811a5e60a06b2e60ba6b6fe14
ajs2104@gmail.com <ajs2104@gmail.com>
2021-01-20 20:47:15 -0300

    Repository Pattern e Unit of Work

---
+++ MyStore.Infra/Respositories/Account/UserRepository.cs
@@ -0,0 +1,42 @@
+﻿using MyStore.Domain.Account.Entities;
+using MyStore.Domain.Account.Repositories;
+using MyStore.Domain.Account.Specs;
+using MyStore.Infra.Persistance.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStore.Infra.Respositories.Account
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly MyStoreDataContext _context;
+
+        public UserRepository(MyStoreDataContext context)
+        {
+            _context = context;
+        }
+
+        public User GetByAuthorizationCode(string authorizationCode)
+        {
+            return _context.Users.Where(UserSpecs.GetByAutorizationCode(authorizationCode)).FirstOrDefault();
+        }
+
+        public User GetUserByUserName(string userName)
+        {
+            return _context.Users.Where(UserSpecs.GetUserByName(userName)).FirstOrDefault();
+        }
+
+        public void Save(User user)
+        {
+            _context.Users.Add(user);
+        }
+
+        public void Update(User user)
+        {
+            _context.Entry<User>(user).State = System.Data.Entity.EntityState.Modified;
+        }
+    }
+}
---
+++ MyStore.Infra/Transaction/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using MyStore.Infra.Persistance.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStore.Infra.Transaction
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly MyStoreDataContext _context;
+
+        public UnitOfWork(MyStoreDataContext context)
+        {
+            _context = context;
+        }
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Rollback()
+        {
+            // Nada :)
+        }
+    }
+}
