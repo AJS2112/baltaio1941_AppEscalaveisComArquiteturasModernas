4bddbd0076dc77a1babee10884a9723901887e1c
ajs2104@gmail.com <ajs2104@gmail.com>
2021-01-20 22:38:24 -0300

    Adicionando Api

---
+++ MyStore.Api/Startup.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Owin.Cors;
+using Newtonsoft.Json;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace MyStore.Api
+{
+    public class Startup
+    {
+        public void Configurarion(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            //var container = new UnityContainer();
+
+            //ConfigureDependencyInjection(config, container);
+            ConfigureWebApi(config);
+
+            app.UseCors(CorsOptions.AllowAll);
+            app.UseWebApi(config);
+        }
+        public static void ConfigureWebApi(HttpConfiguration config)
+        {
+            var formatters = config.Formatters;
+            formatters.Remove(formatters.XmlFormatter);
+
+            var jsonSettings = formatters.JsonFormatter.SerializerSettings;
+            jsonSettings.Formatting = Formatting.Indented;
+            jsonSettings.ContractResolver = new Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver();
+
+            formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+            formatters.JsonFormatter.SerializerSettings.PreserveReferencesHandling = PreserveReferencesHandling.None;
+
+            config.MapHttpAttributeRoutes();
+            config.Routes.MapHttpRoute("DefaultApi", "api/{controller}/{id}", new { id = RouteParameter.Optional });
+        }
+    
+        
+    }
+}
