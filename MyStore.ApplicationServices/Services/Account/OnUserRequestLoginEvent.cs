6c9b836a866e6254f086288aea57299cd79a952d
ajs2104@gmail.com <ajs2104@gmail.com>
2021-01-20 21:27:26 -0300

    ApplicationServices

---
+++ MyStore.ApplicationServices/Services/Account/OnUserRequestLoginEvent.cs
@@ -0,0 +1,14 @@
+﻿using MyStore.Domain.Account.Entities;
+
+namespace MyStore.ApplicationServices.Services.Account
+{
+    internal class OnUserRequestLoginEvent
+    {
+        private User user;
+
+        public OnUserRequestLoginEvent(User user)
+        {
+            this.user = user;
+        }
+    }
+}
